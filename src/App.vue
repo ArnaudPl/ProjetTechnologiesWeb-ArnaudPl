bf5c649bd128bd8ff5da1e7dc1bea8e54bf1d7c2
Arnaud Plumez <arnaud.plumez@he-arc.ch>
2017-11-29 09:14:57 +0100

    Création du layout de base

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,55 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
+  <v-app dark>
+
+    <v-navigation-drawer fixed :mini-variant="miniVariant" app>
       <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-        >
+        <v-list-tile v-for="(item, i) in items" :key="i" value="true">
+
           <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
+            <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
+
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
+          
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+      
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+
     </v-toolbar>
+
     <v-content>
       <v-container fluid>
+
         <v-slide-y-transition mode="out-in">
+
           <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
+
+              <router-view></router-view>
+
           </v-layout>
+          
         </v-slide-y-transition>
+
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      fixed
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+
+    <v-footer app>
       <span>&copy; 2017</span>
+      <v-spacer></v-spacer>
+      <span>Tiago Rodrigues et Arnaud Plumez</span>
+      <v-spacer></v-spacer>
+      <span>HEG</span>
     </v-footer>
+
   </v-app>
 </template>
 
@@ -94,19 +57,14 @@
 export default {
     data () {
         return {
-            clipped: false,
-            drawer: true,
-            fixed: false,
             items: [
                 {
-                    icon: 'bubble_chart',
+                    icon: 'email',
                     title: 'Inspire'
                 }
             ],
-            miniVariant: false,
-            right: true,
-            rightDrawer: false,
-            title: 'Vuetify.js'
+            miniVariant: true,
+            title: 'Le projet incroyable'
         };
     }
 };
