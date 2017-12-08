ee4fff7b73f8bc42b0b5b1412a9d9a8fd18d0efa
Arnaud Plumez <arnaud.plumez@he-arc.ch>
2017-12-08 06:21:59 -0800

    Préparation du projet pour le déploiement sur gh-pages

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/projet-tiago_arnaud/',
 
     /**
      * Source Maps
