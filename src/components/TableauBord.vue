777f450ef1fa76d55cf1b787e749715b11f7f6ed
Arnaud Plumez <arnaud.plumez@he-arc.ch>
2018-01-15 13:53:19 +0100

    Simplification du simulateur de notes

--- src/components/TableauBord.vue
+++ src/components/TableauBord.vue
@@ -13,13 +13,13 @@
                                     <h4>
                                         Choisissez un module :
                                     </h4>
-                                    <v-select @input="getUEofSelectedModule" :items="modules" item-text="name" item-value="id" v-model="selectedModule"></v-select>
+                                    <v-select @input="handleNewNote" :items="organizedModules" item-text="name" item-value="id" v-model="selectedModule"></v-select>
                                     
-                                    <div v-show="selectedModule !== -1 && ue.length > 0">
+                                    <div v-if="selectedModule !== -1 && organizedModules.find(el => el.id === selectedModule).UE">
                                         <h4>
                                             Choisissez une unité d'enseignement :
                                         </h4>
-                                        <v-select @input="inputUE" :items="ue" item-text="name" item-value="id" v-model="selectedUE"></v-select>
+                                        <v-select @input="handleNewNote" :items="organizedModules.find(el => el.id === selectedModule).UE" item-text="name" item-value="id" v-model="selectedUE"></v-select>
                                     </div>
 
                                     <h4>
@@ -27,26 +27,27 @@
                                     </h4>
                                     <v-text-field @input="handleNewNote" v-model="noteSimulee" :rules="noteSimuleeRules"></v-text-field>
                                 </v-flex>
+
                                 <v-flex xs12 lg6>
-                                    <div class="text-xs-center" v-show="selectedModule !== -1 && (ue.length === 0 || selectedUE !== -1)">
+                                    <div class="text-xs-center" v-if="selectedModule !== -1">
                                         <div class="titre-moyenne">Moyenne du module :</div>
-                                        <div class="moyenne" :class="(moyenneModule < 4 ? 'red' : moyenneModule === 4 ? 'yellow' : 'green') + '--text'">{{ moyenneModule === -1 ? 'Aucune note n\'a été ajoutée' : moyenneModule.toFixed(2) }}</div>
+                                        <div class="moyenne" :class="(organizedModules.find(el => el.id === selectedModule).moyenne < 4 ? 'red' : organizedModules.find(el => el.id === selectedModule).moyenne === 4 ? 'yellow' : 'green') + '--text'">{{ isNaN(organizedModules.find(el => el.id === selectedModule).moyenne) ? 'Aucune note n\'a été ajoutée' : organizedModules.find(el => el.id === selectedModule).moyenne }}</div>
                                     </div>
 
-                                    <div class="text-xs-center mt-3" v-show="selectedUE !== -1">
+                                    <div class="text-xs-center mt-3" v-if="selectedModule !== -1 && selectedUE !== -1">
                                         <div class="titre-moyenne">Moyenne de l'unité d'enseignement :</div>
-                                        <div class="moyenne" :class="(moyenneUE < 4 ? 'red' : moyenneUE === 4 ? 'yellow' : 'green') + '--text'">{{ moyenneUE === -1 ? 'Aucune note n\'a été ajoutée' : moyenneUE.toFixed(2) }}</div>
+                                        <div class="moyenne" :class="(organizedModules.find(el => el.id === selectedModule).UE.find(el => el.id === selectedUE).moyenne < 4 ? 'red' : organizedModules.find(el => el.id === selectedModule).UE.find(el => el.id === selectedUE).moyenne === 4 ? 'yellow' : 'green') + '--text'">{{ isNaN(organizedModules.find(el => el.id === selectedModule).UE.find(el => el.id === selectedUE).moyenne) ? 'Aucune note n\'a été ajoutée' : organizedModules.find(el => el.id === selectedModule).UE.find(el => el.id === selectedUE).moyenne }}</div>
                                     </div>
 
-                                    <div class="text-xs-center mt-3" v-show="moyenneModuleSimulee > -1">
+                                    <div class="text-xs-center mt-3" v-if="moduleWithNewNote.length > 0">
                                         <v-divider class="mb-3"></v-divider>
                                         <div class="titre-moyenne">Moyenne simulée du module :</div>
-                                        <div class="moyenne" :class="(moyenneModuleSimulee < 4 ? 'red' : moyenneModuleSimulee === 4 ? 'yellow' : 'green') + '--text'">{{ moyenneModuleSimulee.toFixed(2) }}</div>
+                                        <div class="moyenne">{{ moduleWithNewNote[0].moyenne }}</div>
                                     </div>
 
-                                    <div class="text-xs-center mt-3" v-show="moyenneUESimulee > -1">
+                                    <div class="text-xs-center mt-3" v-if="moduleWithNewNote.length > 0 && selectedUE !== -1">
                                         <div class="titre-moyenne">Moyenne simulée de l'unité d'enseignement :</div>
-                                        <div class="moyenne" :class="(moyenneUESimulee < 4 ? 'red' : moyenneUESimulee === 4 ? 'yellow' : 'green') + '--text'">{{ moyenneUESimulee.toFixed(2) }}</div>
+                                        <div class="moyenne">{{ moduleWithNewNote[0].UE.find(el => el.id === selectedUE).moyenne }}</div>
                                     </div>
                                 </v-flex>
                             </v-layout>
@@ -62,155 +63,101 @@
 export default {
     data () {
         return {
-            modules: [],
+            organizedModules: [],
             selectedModule: -1,
-            ue: [],
             selectedUE: -1,
-            moyenneModule: -1,
-            moyenneUE: -1,
             noteSimulee: null,
-            moyenneModuleSimulee: -1,
-            moyenneUESimulee: -1,
             noteSimuleeRules: [
                 // eslint-disable-next-line
                 (v) => (parseFloat(v) >= 1. && parseFloat(v) <= 6. && ((parseFloat(v) * 100) % 5) / 100 === 0) || 'La note doit être entre 1 et 6 et être un multiple de 0.05'
-            ]
+            ],
+            moduleWithNewNote: []
         };
     },
     methods: {
-        getModulesList () {
-            let modules = localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules'));
-            this.modules = [];
+        getFormattedModules (modules) {
+            let organizedModules = [];
 
             modules.forEach(mod => {
-                this.modules.push({
-                    name: mod.name,
-                    id: mod.id
-                });
-            });
-        },
-        getUEofSelectedModule () {
-            let modules = localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules'));
-            this.ue = [];
-            this.selectedUE = -1;
-            let currentModule;
+                let organizedUE = [];
+                let tmpModule = {};
+                tmpModule.id = mod.id;
+                tmpModule.name = mod.name;
 
-            modules.forEach(mod => {
-                if (mod.id === this.selectedModule) {
-                    currentModule = mod;
-                    if (mod.UE && mod.UE.length > 0) {
-                        mod.UE.forEach(ue => {
-                            this.ue.push({
-                                name: ue.name,
-                                id: ue.id
-                            });
-                        });
-                    }
-                }
-            });
-
-            this.moyenneModule = this.getMoyenneModuleSansUE(currentModule);
-        },
-        inputUE () {
-            let modules = localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules'));
-            let moyennesUE = [];
-            this.moyenneUE = -1;
-
-            modules.forEach(mod => {
-                if (mod.id === this.selectedModule) {
+                if (mod.UE && mod.UE.length > 0) {
                     mod.UE.forEach(ue => {
-                        let moyenneUE = this.getMoyenneUE(ue);
-                        if (moyenneUE !== -1) {
-                            for (let i = 0; i <= parseFloat(ue.coefficient); i += 0.05) {
-                                moyennesUE.push(moyenneUE);
-                            }
+                        let tmpUE = {};
+                        tmpUE.id = ue.id;
+                        tmpUE.name = ue.name;
+                        tmpUE.coefficient = ue.coefficient;
 
-                            if (ue.id === this.selectedUE) {
-                                this.moyenneUE = moyenneUE;
-                            }
+                        if (ue.notes && ue.notes.length > 0) {
+                            let sommeNotes = 0;
+                            ue.notes.forEach(note => {
+                                sommeNotes += parseFloat(note.value);
+                            });
+                            tmpUE.moyenne = (sommeNotes / ue.notes.length).toFixed(2);
+                        }
+
+                        organizedUE.push(tmpUE);
+                    });
+                    tmpModule.UE = organizedUE;
+
+                    // Ajoute toutes les moyennes des UE du module dans un tableau en tenant compte du coefficient
+                    let moyennesUE = [];
+                    organizedUE.forEach(ue => {
+                        if (ue.moyenne) {
+                            for (let i = 0; i <= parseFloat(ue.coefficient); i += 0.05) moyennesUE.push(parseFloat(ue.moyenne));
                         }
                     });
+
+                    // Calcule la moyenne générale du module
+                    let sommeMoyennesUE = 0;
+                    moyennesUE.forEach(moyenne => {
+                        sommeMoyennesUE += parseFloat(moyenne);
+                    });
+                    tmpModule.moyenne = (sommeMoyennesUE / moyennesUE.length).toFixed(2);
+                    organizedModules.push(tmpModule);
+                } else {
+                    if (mod.notes && mod.notes.length > 0) {
+                        let sommeNotes = 0;
+                        mod.notes.forEach(note => {
+                            sommeNotes += parseFloat(note.value);
+                        });
+                        tmpModule.moyenne = (sommeNotes / mod.notes.length).toFixed(2);
+                    }
+                    organizedModules.push(tmpModule);
                 }
             });
-            let sommeMoyennesUE = 0;
-            moyennesUE.forEach(moy => {
-                sommeMoyennesUE += moy;
-            });
-
-            if (moyennesUE.length) this.moyenneModule = sommeMoyennesUE / moyennesUE.length;
-            else this.moyenneModule = -1;
+            return organizedModules;
         },
         handleNewNote () {
-            let moyenneSimUE = -1;
-            let moyenneSimModule = -1;
-            let moyennesUE = [];
-
-            if (parseFloat(this.noteSimulee) >= 1 && this.selectedModule > -1 && this.noteSimuleeRules[0](this.noteSimulee) === true) {
-                let modules = localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules'));
-
-                modules.forEach(mod => {
-                    if (mod.id === this.selectedModule) {
-                        if (mod.UE && mod.UE.length > 0 && this.selectedUE > -1) {
-                            mod.UE.forEach(ue => {
-                                if (ue.id === this.selectedUE) {
-                                    if (!ue.notes) ue.notes = [];
-                                    ue.notes.push({ value: parseFloat(this.noteSimulee) });
-                                    moyenneSimUE = this.getMoyenneUE(ue);
-                                    if (moyenneSimUE !== -1) {
-                                        for (let i = 0; i <= parseFloat(ue.coefficient); i += 0.05) moyennesUE.push(moyenneSimUE);
-                                    }
-                                } else {
-                                    let moyenneUE = this.getMoyenneUE(ue);
-                                    if (moyenneUE !== -1) {
-                                        for (let i = 0; i <= parseFloat(ue.coefficient); i += 0.05) moyennesUE.push(moyenneUE);
-                                    }
-                                }
-                            });
-                        }
+            this.moduleWithNewNote = [];
+            // Vérifie que la note saisie est valide
+            if (this.noteSimuleeRules[0](this.noteSimulee) === true) {
+                // Vérifie qu'un module ait été sélectionné
+                if (this.selectedModule !== -1) {
+                    let modules = localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules'));
+                    // Vérifie que l'utilisateur ait sélectionné une UE si le module en possède
+                    if (this.organizedModules.find(el => el.id === this.selectedModule).UE.length > 0 && this.selectedUE !== -1) {
+                        modules.find(el => el.id === this.selectedModule).UE.find(el => el.id === this.selectedUE).notes.push({ value: this.noteSimulee });
+                    } else if (!this.organizedModules.find(el => el.id === this.selectedModule).UE || this.organizedModules.find(el => el.id === this.selectedModule).UE.length === 0) {
+                        modules.find(el => el.id === this.selectedModule).notes.push({ value: this.noteSimulee });
+                    } else {
+                        return;
                     }
-                });
-                let sommeMoyennesUE = 0;
-                moyennesUE.forEach(moyenne => {
-                    sommeMoyennesUE += moyenne;
-                });
-
-                if (moyennesUE.length) moyenneSimModule = sommeMoyennesUE / moyennesUE.length;
-                else moyenneSimModule = -1;
-            }
-
-            this.moyenneModuleSimulee = moyenneSimModule;
-            this.moyenneUESimulee = moyenneSimUE;
-        },
-        getMoyenneUE (ue) {
-            let sommeNotes = 0;
-            if (ue.notes && ue.notes.length > 0) {
-                ue.notes.forEach(note => {
-                    sommeNotes += note.value;
-                });
-                return sommeNotes / ue.notes.length;
-            } else {
-                return -1;
-            }
-        },
-        getMoyenneModuleSansUE (mod) {
-            if (this.ue.length === 0 && mod.notes && mod.notes.length > 0) {
-                let sommeNotes = 0;
-
-                mod.notes.forEach(note => {
-                    sommeNotes += note.value;
-                });
-                return sommeNotes / mod.notes.length;
-            } else {
-                return -1;
+                    modules = this.getFormattedModules([modules.find(el => el.id === this.selectedModule)]);
+                    this.moduleWithNewNote = modules;
+                }
             }
         }
     },
     mounted: function () {
-        this.getModulesList();
+        this.organizedModules = this.getFormattedModules(localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules')));
     },
     beforeRouteUpdate (to, from, next) {
         if (to.name === 'Tableau de bord') {
-            this.getModulesList();
+            this.organizedModules = this.getFormattedModules(localStorage.getItem('modules') === null ? [] : JSON.parse(localStorage.getItem('modules')));
         }
         next();
     }
